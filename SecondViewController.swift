a192384982512b7dde7302d9947e9d453f258e8e
Kian Mokhlesi <kianmokhlesi@gmail.com>
2020-07-30 11:09:09 -0500

    Set up Storyboard

---
+++ SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  Top Stories
+//
+//  Created by Susan Sam on 7/30/20.
+//  Copyright © 2020 Kian Mokhlesi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIGridLabel {
+
+}
